8a2176685a4c0014075b76dcc9bd3fef1d59820d
kcabo11 <71565123+kcabo11@users.noreply.github.com>
2023-11-13 21:38:50 -0700

    Implemented vision from "Learn Java for FTC" and also from FTCChad.com

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionProcessorTestFromBook.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.processors.FirstVisionProcessor;
+import org.firstinspires.ftc.vision.VisionPortal;
+
+@Autonomous()
+public class VisionProcessorTestFromBook extends OpMode {
+
+    private FirstVisionProcessor visionProcessor;
+    private VisionPortal visionPortal;
+
+    @Override
+    public void init() {
+        visionProcessor = new FirstVisionProcessor();
+        visionPortal = VisionPortal.easyCreateWithDefaults(hardwareMap.get(WebcamName.class, "Webcam 1"), visionProcessor);
+
+    }
+
+    @Override
+    public void init_loop() {
+    }
+
+    @Override
+    public void start() {
+        visionPortal.stopStreaming();
+    }
+
+
+    @Override
+    public void loop() {
+        telemetry.addData("Identified", visionProcessor.getSelection());
+//        switch (visionProcessor.getSelection()) {
+//            case LEFT:
+//            case NONE:
+//                board.setServoPosition(0);
+//                break;
+//            case MIDDLE:
+//                board.setServoPosition(90);
+//                break;
+//            case RIGHT:
+//                board.setServoPosition(180);
+//                break;
+        telemetry.update();
+        }
+    }
+
+
