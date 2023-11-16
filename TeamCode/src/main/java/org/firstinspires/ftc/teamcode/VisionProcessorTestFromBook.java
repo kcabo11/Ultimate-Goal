d319fcdfea6584d9cc8fb3c3176d9776839ce85f
kcabo11 <71565123+kcabo11@users.noreply.github.com>
2023-11-15 17:22:25 -0700

    Does not crash, runs middle fairly well. april tags need camera calibration

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionProcessorTestFromBook.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionProcessorTestFromBook.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -10,7 +11,10 @@ import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.firstinspires.ftc.teamcode.processors.FirstVisionProcessor;
@@ -18,11 +22,15 @@ import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 @Autonomous()
 public class VisionProcessorTestFromBook extends LinearOpMode {
 
     private FirstVisionProcessor visionProcessor;
     private VisionPortal visionPortal;
+    private VisionPortal visionPortal_2;
     private DcMotor leftFront = null;
     private DcMotor rightFront = null;
     private DcMotor leftBack = null;
@@ -31,8 +39,8 @@ public class VisionProcessorTestFromBook extends LinearOpMode {
     private IMU imu = null;      // Control/Expansion Hub IMU
 
     static final double FORWARD_SPEED = 0.3;
+    static final double REVERSE_SPEED = -0.3;
     static final double TURN_SPEED = 0.3;
-
     private ElapsedTime runtime = new ElapsedTime();
 
 
@@ -47,12 +55,19 @@ public class VisionProcessorTestFromBook extends LinearOpMode {
     final double MAX_AUTO_TURN = 0.3;   //  Clip the turn speed to this max value (adjust for your robot)
 
     private static final boolean USE_WEBCAM = true;  // Set true to use a webcam, or false for a phone camera
-    private static final int DESIRED_TAG_ID = 1;     // Choose the tag you want to approach or set to -1 for ANY tag.
+    private static int DESIRED_TAG_ID = 1;     // Choose the tag you want to approach or set to -1 for ANY tag.
     private AprilTagProcessor aprilTag;              // Used for managing the AprilTag detection process.
     private AprilTagDetection desiredTag = null;     // Used to hold the data for a detected AprilTag
+    private boolean targetFound = false;    // Set to true when an AprilTag target is detected
+    private double drive = 0;        // Desired forward power/speed (-1 to +1)
+    private double strafe = 0;        // Desired strafe power/speed (-1 to +1)
+    private double turn = 0;        // Desired turning power/speed (-1 to +1)
 
     @Override
     public void runOpMode() {
+
+        // Initialize the Apriltag Detection process and Webcam 2
+        initAprilTag();
 
         visionProcessor = new FirstVisionProcessor();
         visionPortal = VisionPortal.easyCreateWithDefaults(hardwareMap.get(WebcamName.class, "Webcam 1"), visionProcessor);
@@ -62,10 +77,6 @@ public class VisionProcessorTestFromBook extends LinearOpMode {
         telemetry.addData("RectMiddle Saturation: ", visionProcessor.satRectMiddle);
         telemetry.addData("Average Saturation: ", visionProcessor.satAverage);
 
-        boolean targetFound = false;    // Set to true when an AprilTag target is detected
-        double drive = 0;        // Desired forward power/speed (-1 to +1)
-        double strafe = 0;        // Desired strafe power/speed (-1 to +1)
-        double turn = 0;        // Desired turning power/speed (-1 to +1)
 
         // Initialize the drive system variables.
         leftFront = hardwareMap.get(DcMotor.class, "leftFront");
@@ -130,7 +141,7 @@ public class VisionProcessorTestFromBook extends LinearOpMode {
         // =========================================================================================
 
 
-        if (false){//(visionProcessor.getSelection() == FirstVisionProcessor.Selected.NONE) {
+        if (true) { //(visionProcessor.getSelection() == FirstVisionProcessor.Selected.MIDDLE) {
             // ASSUMING SPIKE MARK IS MIDDLE POSITION
             // ==================================== Step 1: Drive forward for 2 seconds ====================================
 
@@ -144,17 +155,34 @@ public class VisionProcessorTestFromBook extends LinearOpMode {
 //            leftBack.setDirection(DcMotor.Direction.FORWARD);
 //            rightBack.setDirection(DcMotor.Direction.FORWARD);
 
-            // Step 1:  Drive forward for 3 seconds
-            leftFront.setPower(FORWARD_SPEED);
-            rightFront.setPower(FORWARD_SPEED);
+            // Step 1.a:  Drive forward for __ seconds
+            leftFront.setPower(-FORWARD_SPEED);
+            rightFront.setPower(-FORWARD_SPEED);
             rightBack.setPower(FORWARD_SPEED);
             leftBack.setPower(FORWARD_SPEED);
             runtime.reset();
-            while (opModeIsActive() && (runtime.seconds() < 1.0)) {
+            while (opModeIsActive() && runtime.seconds() < 1) {
                 telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
                 telemetry.update();
             }
+            leftFront.setPower(0);
+            rightFront.setPower(0);
+            rightBack.setPower(0);
+            leftBack.setPower(0);
+
             sleep(1000);
+
+//            // Step 1.b:  Drive backward for __ seconds
+//            leftFront.setPower(REVERSE_SPEED);
+//            rightFront.setPower(REVERSE_SPEED);
+//            rightBack.setPower(REVERSE_SPEED);
+//            leftBack.setPower(REVERSE_SPEED);
+//            runtime.reset();
+//            while (opModeIsActive() && (runtime.seconds() < .01)) {
+//                telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
+//                telemetry.update();
+//            }
+//            sleep(2000);
 //
 //            // Step 3:  Drive Backward for 1 Second
 //            leftDrive.setPower(-FORWARD_SPEED);
@@ -174,7 +202,7 @@ public class VisionProcessorTestFromBook extends LinearOpMode {
 //            sleep(1000);
 
 
-            // ==================================== Step 3: Turn right 90 degrees by gyro ====================================
+            // ==================================== Step 3: Turn right 90 degrees ====================================
 
             //Spin left for 1.3 seconds
             leftFront.setPower(TURN_SPEED);
@@ -182,75 +210,194 @@ public class VisionProcessorTestFromBook extends LinearOpMode {
             rightBack.setPower(-TURN_SPEED);
             leftBack.setPower(TURN_SPEED);
             runtime.reset();
-            while (opModeIsActive() && (runtime.seconds() < .6)) {
+            while (opModeIsActive() && (runtime.seconds() < 1.5)) {
                 telemetry.addData("Path", "Leg 2: %4.1f S Elapsed", runtime.seconds());
                 telemetry.update();
             }
 
-            // ==================================== Step 4: Use the automatic code to drive the qr code ======================
-            //** Insert omni wheel logitech camera code here **
-            // If Left Bumper is being pressed, AND we have found the desired target, Drive to target Automatically .
-//            if (gamepad1.left_bumper && targetFound) {
-//            if (targetFound) {
-//
-//                // Determine heading, range and Yaw (tag image rotation) error so we can use them to control the robot automatically.
-//                double  rangeError      = (desiredTag.ftcPose.range - DESIRED_DISTANCE);
-//                double  headingError    = desiredTag.ftcPose.bearing;
-//                double  yawError        = desiredTag.ftcPose.yaw;
-//
-//                // Use the speed and turn "gains" to calculate how we want the robot to move.
-//                drive  = Range.clip(rangeError * SPEED_GAIN, -MAX_AUTO_SPEED, MAX_AUTO_SPEED);
-//                turn   = Range.clip(headingError * TURN_GAIN, -MAX_AUTO_TURN, MAX_AUTO_TURN) ;
-//                strafe = Range.clip(-yawError * STRAFE_GAIN, -MAX_AUTO_STRAFE, MAX_AUTO_STRAFE);
-//
-//                telemetry.addData("Auto","Drive %5.2f, Strafe %5.2f, Turn %5.2f ", drive, strafe, turn);
-//            } else {
-//
-//                // drive using manual POV Joystick mode.  Slow things down to make the robot more controlable.
-//                drive  = -gamepad1.left_stick_y  / 2.0;  // Reduce drive rate to 50%.
-//                strafe = -gamepad1.left_stick_x  / 2.0;  // Reduce strafe rate to 50%.
-//                turn   = -gamepad1.right_stick_x / 3.0;  // Reduce turn rate to 33%.
-//                telemetry.addData("Manual","Drive %5.2f, Strafe %5.2f, Turn %5.2f ", drive, strafe, turn);
+            // Drive towards Backdrop
+            // Step 1:  Drive forward for __ seconds
+//            leftFront.setPower(REVERSE_SPEED);
+//            rightFront.setPower(REVERSE_SPEED);
+//            rightBack.setPower(REVERSE_SPEED);
+//            leftBack.setPower(REVERSE_SPEED);
+//            runtime.reset();
+//            while (opModeIsActive() && (runtime.seconds() < .4)) {
+//                telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
+//                telemetry.update();
 //            }
-//            telemetry.update();
-//
-//            // Apply desired axes motions to the drivetrain.
-//            moveRobot(drive, strafe, turn);
-//            sleep(10);
-//        }
+//            sleep(1000);
+
+            // ==================================== Step 4: Use the automatic code to drive the qr code ======================
+            DESIRED_TAG_ID = 2;
+            driveToAprilTag();
+        }
+    }
+
+    private void initAprilTag() {
+        // Create the AprilTag processor by using a builder.
+        aprilTag = new AprilTagProcessor.Builder().build();
+
+        // Adjust Image Decimation to trade-off detection-range for detection-rate.
+        // eg: Some typical detection data using a Logitech C920 WebCam
+        // Decimation = 1 ..  Detect 2" Tag from 10 feet away at 10 Frames per second
+        // Decimation = 2 ..  Detect 2" Tag from 6  feet away at 22 Frames per second
+        // Decimation = 3 ..  Detect 2" Tag from 4  feet away at 30 Frames Per Second
+        // Decimation = 3 ..  Detect 5" Tag from 10 feet away at 30 Frames Per Second
+        // Note: Decimation can be changed on-the-fly to adapt during a match.
+        aprilTag.setDecimation(2);
+
+        // Create the vision portal by using a builder.
+        visionPortal_2 = new VisionPortal.Builder()
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 2"))
+                .addProcessor(aprilTag)
+                .build();
+    }
+
+    /**
+     * Move robot according to desired axes motions
+     * <p>
+     * Positive X is forward
+     * <p>
+     * Positive Y is strafe left
+     * <p>
+     * Positive Yaw is counter-clockwise
+     */
+    public void moveRobot(double x, double y, double yaw) {
+        // Calculate wheel powers.
+        double leftFrontPower    =  x -y -yaw;
+        double rightFrontPower   =  x +y +yaw;
+        double leftBackPower     =  x +y -yaw;
+        double rightBackPower    =  x -y +yaw;
+
+        // Normalize wheel powers to be less than 1.0
+        double max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+        max = Math.max(max, Math.abs(leftBackPower));
+        max = Math.max(max, Math.abs(rightBackPower));
+
+        if (max > 1.0) {
+            leftFrontPower /= max;
+            rightFrontPower /= max;
+            leftBackPower /= max;
+            rightBackPower /= max;
+        }
+
+        // Send powers to the wheels.
+        leftFront.setPower(leftFrontPower);
+        rightFront.setPower(rightFrontPower);
+        leftBack.setPower(leftBackPower);
+        rightBack.setPower(rightBackPower);
+    }
+
+    /*
+     Manually set the camera gain and exposure.
+     This can only be called AFTER calling initAprilTag(), and only works for Webcams;
+    */
+    private void    setManualExposure(int exposureMS, int gain) {
+        // Wait for the camera to be open, then use the controls
+
+        if (visionPortal == null) {
+            return;
+        }
+
+        // Make sure camera is streaming before we try to set the exposure controls
+        if (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING) {
+            telemetry.addData("Camera", "Waiting");
+            telemetry.update();
+            while (!isStopRequested() && (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING)) {
+                sleep(20);
+            }
+            telemetry.addData("Camera", "Ready");
+            telemetry.update();
+        }
+
+        // Set camera controls unless we are stopping.
+        if (!isStopRequested())
+        {
+            ExposureControl exposureControl = visionPortal.getCameraControl(ExposureControl.class);
+            if (exposureControl.getMode() != ExposureControl.Mode.Manual) {
+                exposureControl.setMode(ExposureControl.Mode.Manual);
+                sleep(50);
+            }
+            exposureControl.setExposure((long)exposureMS, TimeUnit.MILLISECONDS);
+            sleep(20);
+            GainControl gainControl = visionPortal.getCameraControl(GainControl.class);
+            gainControl.setGain(gain);
+            sleep(20);
+        }
+    }
+
+
+    private void driveToAprilTag() {
+
+        targetFound = false;
+        desiredTag = null;
+        double rangeError;
+        double headingError;
+        double yawError;
+
+        while (true) {
+            // Step through the list of detected tags and look for a matching tag
+            List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+            for (AprilTagDetection detection : currentDetections) {
+                // Look to see if we have size info on this tag.
+                if (detection.metadata != null) {
+                    //  Check to see if we want to track towards this tag.
+                    if ((DESIRED_TAG_ID < 0) || (detection.id == DESIRED_TAG_ID)) {
+                        // Yes, we want to use this tag.
+                        targetFound = true;
+                        desiredTag = detection;
+                        break;  // don't look any further.
+                    } else {
+                        // This tag is in the library, but we do not want to track it right now.
+                        telemetry.addData("Skipping", "Tag ID %d is not desired", detection.id);
+                    }
+                } else {
+                    // This tag is NOT in the library, so we don't have enough information to track to it.
+                    telemetry.addData("Unknown", "Tag ID %d is not in TagLibrary", detection.id);
+                }
+            }
+
+            // Tell the driver what we see, and what to do.
+            if (targetFound) {
+                telemetry.addData("\n>", "HOLD Left-Bumper to Drive to Target\n");
+                telemetry.addData("Found", "ID %d (%s)", desiredTag.id, desiredTag.metadata.name);
+                telemetry.addData("Range", "%5.1f inches", desiredTag.ftcPose.range);
+                telemetry.addData("Bearing", "%3.0f degrees", desiredTag.ftcPose.bearing);
+                telemetry.addData("Yaw", "%3.0f degrees", desiredTag.ftcPose.yaw);
+            } else {
+                telemetry.addData("\n>", "Drive using joysticks to find valid target\n");
+            }
+
+            // If Left Bumper is being pressed, AND we have found the desired target, Drive to target Automatically .
+            //            if (gamepad1.left_bumper && targetFound) {
+            if (targetFound) { //(targetFound) {
+
+                // Determine heading, range and Yaw (tag image rotation) error so we can use them to control the robot automatically.
+                rangeError = (desiredTag.ftcPose.range - DESIRED_DISTANCE);
+                headingError = desiredTag.ftcPose.bearing;
+                yawError = desiredTag.ftcPose.yaw;
+
+                // Use the speed and turn "gains" to calculate how we want the robot to move.
+                drive = Range.clip(rangeError * SPEED_GAIN, -MAX_AUTO_SPEED, MAX_AUTO_SPEED);
+                turn = Range.clip(headingError * TURN_GAIN, -MAX_AUTO_TURN, MAX_AUTO_TURN);
+                strafe = Range.clip(-yawError * STRAFE_GAIN, -MAX_AUTO_STRAFE, MAX_AUTO_STRAFE);
+
+                telemetry.addData("Auto", "Drive %5.2f, Strafe %5.2f, Turn %5.2f ", drive, strafe, turn);
+            } else {
+
+                // drive using manual POV Joystick mode.  Slow things down to make the robot more controlable.
+                drive = -gamepad1.left_stick_y / 2.0;  // Reduce drive rate to 50%.
+                strafe = -gamepad1.left_stick_x / 2.0;  // Reduce strafe rate to 50%.
+                turn = -gamepad1.right_stick_x / 3.0;  // Reduce turn rate to 33%.
+                telemetry.addData("Manual", "Drive %5.2f, Strafe %5.2f, Turn %5.2f ", drive, strafe, turn);
+            }
+            telemetry.update();
+
+            // Apply desired axes motions to the drivetrain.
+            moveRobot(drive, strafe, turn);
+            sleep(10);
 
         }
-//}
-
-////    public double getHeading() {
-////        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
-////        return orientation.getYaw(AngleUnit.DEGREES);
-////    }
-//
-//    public void moveRobot(double x, double y, double yaw) {
-//        // Calculate wheel powers.
-//        double leftFrontPower    =  x -y -yaw;
-//        double rightFrontPower   =  x +y +yaw;
-//        double leftBackPower     =  x +y -yaw;
-//        double rightBackPower    =  x -y +yaw;
-//
-//        // Normalize wheel powers to be less than 1.0
-//        double max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
-//        max = Math.max(max, Math.abs(leftBackPower));
-//        max = Math.max(max, Math.abs(rightBackPower));
-//
-//        if (max > 1.0) {
-//            leftFrontPower /= max;
-//            rightFrontPower /= max;
-//            leftBackPower /= max;
-//            rightBackPower /= max;
-//        }
-//
-//        // Send powers to the wheels.
-//        leftFront.setPower(leftFrontPower);
-//        rightFront.setPower(rightFrontPower);
-//        leftBack.setPower(leftBackPower);
-//        rightBack.setPower(rightBackPower);
-//    }
     }
 }
